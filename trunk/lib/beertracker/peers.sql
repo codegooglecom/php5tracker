f393d90ab73fe1294f718e50ad17f61dfebdc572
mypostbox@centrum.cz <mypostbox@centrum.cz@1566b0d7-bb3b-3d57-4cc7-9146607560c4>
2013-08-15 15:59:18 +0000

    Added MEMORY engine

--- trunk/lib/beertracker/peers.sql
+++ trunk/lib/beertracker/peers.sql
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `peers`
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8
 
 
---- MyISAM db engine
+--- InnoDB db engine
 CREATE TABLE IF NOT EXISTS `peers` 
 (
   `info_hash` binary(20) NOT NULL,
@@ -33,3 +33,15 @@ CREATE TABLE IF NOT EXISTS `peers`
   `updated` int(10) unsigned NOT NULL,
   PRIMARY KEY (`info_hash`,`compact`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+
+
+--- MEMORY engine
+CREATE TABLE IF NOT EXISTS `peers`
+(
+  `info_hash` binary(20) NOT NULL,
+  `compact` binary(6) NOT NULL,
+  `state` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `updated` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`info_hash`,`compact`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8
